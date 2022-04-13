bf755b76669ad3ec2071ad1f9c5a7b1ae6c27824
James Pike <james@jpike.ca>
2022-04-12 19:10:32 -0700

    Added multiple enemies and multiple bullets.

--- SpaceInvader3D/MainViewController.h
+++ SpaceInvader3D/MainViewController.h
@@ -11,6 +11,36 @@
 
 @interface MainViewController : GLKViewController
 
+typedef struct Bullet {
+    bool    _drawBullet;
+    float   _bulletXPosition;
+    float   _bulletYPosition;
+    float   _bulletZPosition;
+    float   _bulletXRotation;
+    float   _bulletYRotation;
+    float   _bulletZRotation;
+    float   _bulletXScale;
+    float   _bulletYScale;
+    float   _bulletZScale;
+} Bullet;
+
+typedef struct Alien {
+    bool    _drawAlien;
+    float   _alienXPosition;
+    float   _alienYPosition;
+    float   _alienZPosition;
+    float   _alienXRotation;
+    float   _alienYRotation;
+    float   _alienZRotation;
+    float   _alienXScale;
+    float   _alienYScale;
+    float   _alienZScale;
+    bool    _alienMovingRight;
+    bool    _alienDestroyed;
+} Alien;
+
+@property int aliensToSpawn;
+
 @property (nonatomic, weak) IBOutlet UILabel *scoreLabel;
 @property (nonatomic, weak) IBOutlet UILabel *highScoreLabel;
 
